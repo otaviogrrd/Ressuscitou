e5d8beac6be184031b33b9fb41f1e11a0b733839
Otávio Garrido Moraes <otavio.grrd@gmail.com>
2016-11-24 10:56:28 -0200

    Update cantoslist.js

--- js/cantoslist.js
+++ js/cantoslist.js
@@ -222,7 +222,9 @@ var myaudio = new Audio();
 function startCont(){
 	$('#music_controls').css({'display': 'block' });	
 	$('#start').css({'display': 'none' });
-	myaudio = new Audio("http://app_access:g23#r2%LP02gt%fsrt@www.cn.org.br/app_ressuscitou/"+url);
+	var allurl = "http://app_access:g23#r2%LP02gt%fsrt@www.cn.org.br/app_ressuscitou/"+url;
+	window.confirm('Erro!\n'+allurl);
+	myaudio = new Audio(allurl);
 	html5audio.play();
 }
 
